e75811d30921c756eff16a7bb51a0807729dcb28
rehan-ankalgi-7t2 <rehanankalgi7t2@gmail.com>
2025-12-02 10:23:47 +0530

    Initial Commit

---
+++ verbadicium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  verbadicium
+//
+//  Created by Rehan on 02/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ verbadicium/verbadiciumApp.swift
@@ -0,0 +1,17 @@
+//
+//  verbadiciumApp.swift
+//  verbadicium
+//
+//  Created by Rehan on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct verbadiciumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
